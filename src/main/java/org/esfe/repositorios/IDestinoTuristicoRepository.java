1b6820b5558fd3696a0555e99f6da22e2831649a
DanielMasin <164657442+DanielMasin@users.noreply.github.com>
2025-07-30 17:14:51 -0600

    destinosturisticos

---
+++ src/main/java/org/esfe/repositorios/IDestinoTuristicoRepository.java
@@ -0,0 +1,7 @@
+package org.esfe.repositorios;
+
+import org.esfe.modelos.DestinoTuristico;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IDestinoTuristicoRepository {
+}
