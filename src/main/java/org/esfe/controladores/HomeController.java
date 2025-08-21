b2c37580d92d182cd6459e2f116445a39c7f7dec
DanielMasin <164657442+DanielMasin@users.noreply.github.com>
2025-08-21 15:12:59 -0600

    cambios en la pagina principal

--- src/main/java/org/esfe/controladores/HomeController.java
+++ src/main/java/org/esfe/controladores/HomeController.java
@@ -1,14 +1,42 @@
 package org.esfe.controladores;
 
+import org.esfe.modelos.DestinoTuristico;
+import org.esfe.modelos.ImagenDTO;
+import org.esfe.servicios.interfaces.IDestinoTuristicoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Base64;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Controller
-@RequestMapping("/")
 public class HomeController {
-    @GetMapping
-    public String index(){
-        return "home/index";
+
+    @Autowired
+    private IDestinoTuristicoService destinoTuristicoService;
+
+    @GetMapping({"/", "/home"})
+    public String index(Model model) {
+            List<DestinoTuristico> destinos = destinoTuristicoService.ObtenerTodo();
+            List<Map<String, Object>> destinosDTO = destinos.stream().map(dest -> {
+                List<ImagenDTO> imagenesDTO = dest.getImagenes().stream()
+                        .map(img -> new ImagenDTO(
+                                img.getId(),
+                                img.getBytesArrayImage() != null ?
+                                        Base64.getEncoder().encodeToString(img.getBytesArrayImage()) : null))
+                        .collect(Collectors.toList());
+                Map<String, Object> dto = new HashMap<>();
+                dto.put("destino", dest);
+                dto.put("imagenes", imagenesDTO);
+                return dto;
+            }).collect(Collectors.toList());
+            model.addAttribute("destinoTuristicos", destinosDTO);
+            return "home/index";
+        }
+
     }
-}
